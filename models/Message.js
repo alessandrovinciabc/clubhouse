28b95b893e4cc5d693dbc5616c9ee0c130c10abf
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-06-11 12:13:10 +0200

    Add timestamps to messages, show them in index view

--- models/Message.js
+++ models/Message.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const { Schema, ObjectId } = mongoose;
 
-const MessageSchema = new Schema({
-  owner: { required: true, type: ObjectId, ref: 'User' },
-  title: { type: String, maxLength: 60, required: true },
-  message: { type: String, maxLength: 280, required: true },
-});
+const MessageSchema = new Schema(
+  {
+    owner: { required: true, type: ObjectId, ref: 'User' },
+    title: { type: String, maxLength: 60, required: true },
+    message: { type: String, maxLength: 280, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Message', MessageSchema);
